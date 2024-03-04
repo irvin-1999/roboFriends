3848762de4211bf44470b77d5001c2403ee82e5d
Boitumelo Iven Moshitwa <itumelo553@gmail.com>
2024-03-04 13:54:24 +0200

    robo friends startpack

---
+++ src/components/Card.js
@@ -0,0 +1,13 @@
+const Card = () => {
+  return (
+    <>
+      <img src="" alt="robot" />
+      <div>
+        <h2>Jane Doe</h2>
+        <p>muemail@gmail.com</p>
+      </div>
+    </>
+  );
+};
+
+export default Card;
