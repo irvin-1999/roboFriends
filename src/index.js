c8dc93a8e68d249e1173def57bcfb0202d810599
Boitumelo Iven Moshitwa <itumelo553@gmail.com>
2024-04-08 20:29:23 +0200

    add redux code,actions and reducers

--- src/index.js
+++ src/index.js
@@ -1,17 +1,7 @@
 import React from "react";
-import { Provider, connect, createStoreHook } from "react-redux";
-import { createStore } from "redux";
-import { searchRobots } from "./redux/reducers";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-const store = createStore(searchRobots);
-// const store = createStoreHook(searchRobots);
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
-);
+root.render(<App />);
--- src/redux/searchContext.js
+++
@@ -1,7 +0,0 @@
-import { createContext } from "react";
-
-const SearchContext = createContext({
-  search: "",
-});
-
-export default SearchContext;
