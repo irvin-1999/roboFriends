5d35944b5ffa2d5a8d12a653802858449f27983d
Boitumelo Iven Moshitwa <itumelo553@gmail.com>
2024-04-10 13:23:01 +0200

    add a logger

--- src/index.js
+++ src/index.js
@@ -6,9 +6,13 @@ import { Provider } from "react-redux";
 import { configureStore } from "@reduxjs/toolkit";
 import App from "./App";
 import "./index.css";
+import { createLogger } from "redux-logger";
+
 const rootReducer = combineReducers({ requestRobots, searchRobots });
+const logger = createLogger();
 const store = configureStore({
   reducer: rootReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
